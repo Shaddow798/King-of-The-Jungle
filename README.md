f8b77de15721f07c64d82877a73f079fdccb25a5
Shaddow798 <37914686+Shaddow798@users.noreply.github.com>
2022-05-17 21:39:20 +0000

    Maintaing things spliting files

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # King of the Jungle
 This is a computer science 
+
+# computerAnimalNumb = 0
+    # computerAnimal = ""
+    # computerAnimalNumb = (random.randint(1,3))
+    # Make the computers choice a animal name rather than a number
+    # if computerAnimalNumb == 1:
+    #    computerAnimal = "lion"
+    # elif computerAnimalNumb == 2:
+    #    computerAnimal = "elephant"
+    # elif computerAnimalNumb == 3:
+    #    computerAnimal = "mouse"
---
+++ utils.py
@@ -0,0 +1,29 @@
+import sys
+import os
+# Clear the screen
+# Operting system detection funciton from stack overflow modifed.
+# https://stackoverflow.com/questions/8220108/how-do-i-check-the-operating-system-in-python
+
+
+def screenClear():
+    if sys.platform == "linux" or sys.platform == "linux2":
+        os.system('clear')
+    elif sys.platform == "darwin":
+        os.system('clear')
+    elif sys.platform == "win32":
+        os.system('cls')
+
+def rulesScreen():
+    print("""The rules of this game are:
+    1. You will be asked to pick an animal.
+    2. The computer will then pick randomly one of the 3 animals.
+    3. If you can the computer both pick the same animal,
+    a message will be displayed saying no battle was fought
+    4. If it is not tied the winner will be decide with these combat rules:
+        Lion beats mouse
+        Mouse beats Elephant
+        Elephant beats lion
+    5. The winner will be displayed and given a point.
+    6. Pressing 0 will display the score and exit.""")
+    input("Press the enter key to continue to the menu: ")
+    screenClear()
